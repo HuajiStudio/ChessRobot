cdc55a821f4f59ebc0da46afbf0509a2a6ec2626
Lasm Gratel <lasm_gratel@hotmail.com>
2017-07-28 21:21:48 +0800

    awesome

--- ChessRobot/ChessRobot.cpp
+++ ChessRobot/ChessRobot.cpp
@@ -1,11 +1,47 @@
-// ChessRobot2.cpp : 定义控制台应用程序的入口点。
+// ChessRobot.cpp : 定义控制台应用程序的入口点。
 //
 
+#include <boost/asio.hpp>
 #include "stdafx.h"
 
+using namespace boost::asio;
 
-int main()
+io_service service;
+ip::tcp::socket sock(service);
+ip::tcp::acceptor acc;
+char buff[512];
+
+const int DEFAULT_PORT = 6489;
+
+int main(int argc, char **argv)
 {
+	ip::tcp::endpoint ep;
+	if (argc == 2)
+		ep = ip::tcp::endpoint(ip::address::from_string("127.0.0.1"), std::stoi(argv[1]));
+	else
+		ep = ip::tcp::endpoint(ip::address::from_string("127.0.0.1"), DEFAULT_PORT);
+	sock.async_connect(ep, connect_handler);
+	sock.async_read_some(buff, read_handler);
+	acc = ip::tcp::acceptor(service, ep);
+	service.run();
     return 0;
 }
 
+void connect_handler(const boost::system::error_code & ec)
+{
+	if (ec != boost::system::errc::success)
+	{
+		fprintf(stderr, "Error when connecting to server: %s", ec.message().c_str());
+		return;
+	}
+}
+
+void read_handler(const boost::system::error_code & error, size_t bytes_transferred)
+{
+	if (error != boost::system::errc::success)
+	{
+		fprintf(stderr, "Error when connecting to server: %s", error.message().c_str());
+		return;
+	}
+	// Do something with buffer
+}
